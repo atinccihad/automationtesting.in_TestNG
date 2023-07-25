d3d5e5ffd0be61476f2b89ce11adfe2863ab9069
atinccihad <atinccihad@gmail.com>
2023-07-25 17:29:54 +0300

    automationtesting 0.24

---
+++ src/test/java/in/automationtesting/tests/p3_myAccountLogin/MyAccountLogin_06.java
@@ -0,0 +1,43 @@
+package in.automationtesting.tests.p3_myAccountLogin;
+
+import in.automationtesting.pages.HomePage;
+import in.automationtesting.utilities.ConfigurationReader;
+import in.automationtesting.utilities.Driver;
+import in.automationtesting.utilities.ReusableMethods;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class MyAccountLogin_06 {
+    HomePage homePage = new HomePage();
+
+    @BeforeClass
+    public void getUrl_login() {
+        //1) Open the browser
+        //2) Enter the URL “http://practice.automationtesting.in/”
+        Driver.getDriver().get(ConfigurationReader.getProperty("base_url"));
+    }
+
+    @Test
+    public void myAccountLogin_06() {
+        //3) Click on My Account Menu
+        Driver.getDriver().navigate().refresh();
+        ReusableMethods.clickWithJS(homePage.myAccountButton);
+        homePage.myAccountButton.click();
+        //4) Enter the password field with some characters.
+        Driver.getDriver().navigate().refresh();
+        ReusableMethods.clickWithJS(homePage.myAccountButton);
+        ReusableMethods.clickWithJS(homePage.passwordBox);
+        homePage.passwordBox.sendKeys("/*+_$", Keys.ENTER);
+        //5) The password field should display the characters
+        // in asterisks or bullets such that the password is not visible on the screen
+        assert homePage.wrongText.isDisplayed();
+        Driver.getDriver().navigate().refresh();
+    }
+
+    @AfterClass
+    public void closeDriver() {Driver.closeDriver();}
+
+}
+
