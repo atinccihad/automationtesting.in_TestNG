5989fe34c1208de625a4d0bd8577e168d2ebaf73
atinccihad <atinccihad@gmail.com>
2023-07-25 21:13:17 +0300

    automationtesting 0.25.1

--- src/test/java/in/automationtesting/tests/p3_myAccountLogin/MyAccountLogin_07.java
+++ src/test/java/in/automationtesting/tests/p3_myAccountLogin/MyAccountLogin_07.java
@@ -3,13 +3,14 @@ package in.automationtesting.tests.p3_myAccountLogin;
 import in.automationtesting.pages.HomePage;
 import in.automationtesting.utilities.ConfigurationReader;
 import in.automationtesting.utilities.Driver;
+import in.automationtesting.utilities.Report;
 import in.automationtesting.utilities.ReusableMethods;
 import org.openqa.selenium.Keys;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class MyAccountLogin_07 {
+public class MyAccountLogin_07 extends Report {
     //25. Login-Handles case sensitive
     HomePage homePage = new HomePage();
 
@@ -21,7 +22,8 @@ public class MyAccountLogin_07 {
     }
 
     @Test
-    public void myAccountLogin_05() {
+    public void myAccountLogin_07() {
+        extentTest = extentReports.createTest("myAccountLogin_07","Gecerli username ve sifre ile giris yapabilmeli.");
         //3) Click on My Account Menu
         Driver.getDriver().navigate().refresh();
         ReusableMethods.clickWithJS(homePage.myAccountButton);
@@ -29,12 +31,16 @@ public class MyAccountLogin_07 {
         //4) Enter the case changed username in username textbox
         Driver.getDriver().navigate().refresh();
         homePage.myAccountButton.click();
+        extentTest.info("myAccountButton is clicked.");
         homePage.emailBox.sendKeys("wrong", Keys.ENTER);
+        extentTest.info("Kullanıcı adı metin kutusuna durum değiştirmeyen kullanıcı adı girildi.");
         //5) Enter the case chenged password in the password tetxbox
         ReusableMethods.clickWithJS(homePage.passwordBox);
         homePage.passwordBox.sendKeys("wrong", Keys.ENTER);
+        extentTest.info("Parola metin kutusuna durum değiştirilen parola girildi.");
         //6) Click on login button
         homePage.loginButton.click();
+        extentTest.info("login button tiklandi.");
         //7) Proper error must be displayed(ie Invalid username) and prompt to enter login again
         assert homePage.wrongText.isDisplayed();
         Driver.getDriver().navigate().refresh();
