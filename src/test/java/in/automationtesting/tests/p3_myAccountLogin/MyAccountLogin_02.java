4d9a279578c7bde37c297a5849c6425c863a15fa
atinccihad <atinccihad@gmail.com>
2023-07-25 22:00:33 +0300

    automationtesting 0.21.1

--- src/test/java/in/automationtesting/tests/p3_myAccountLogin/MyAccountLogin_02.java
+++ src/test/java/in/automationtesting/tests/p3_myAccountLogin/MyAccountLogin_02.java
@@ -4,11 +4,12 @@ import in.automationtesting.pages.HomePage;
 import in.automationtesting.utilities.ConfigurationReader;
 import in.automationtesting.utilities.Driver;
 import in.automationtesting.utilities.ReusableMethods;
+import in.automationtesting.utilities.TestBaseRapor;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class MyAccountLogin_02 {
+public class MyAccountLogin_02 extends TestBaseRapor {
     //20. Log-in with incorrect username and incorrect password
     HomePage homePage = new HomePage();
     @BeforeClass
@@ -20,6 +21,7 @@ public class MyAccountLogin_02 {
 
     @Test
     public void myAccountLogin_02() {
+        extentTest = extentReports.createTest("myAccountLogin_08","Gecersiz username ve sifre ile giris yapilmamali.");
         //3) Click on My Account Menu
         Driver.getDriver().navigate().refresh();
         ReusableMethods.clickWithJS(homePage.myAccountButton);
@@ -27,15 +29,21 @@ public class MyAccountLogin_02 {
         //4) Enter incorrect username in username textbox
         Driver.getDriver().navigate().refresh();
         homePage.myAccountButton.click();
+        extentTest.info("myAccountButton is clicked.");
         homePage.emailBox.sendKeys("wrongMail");
+        extentTest.info("yanlis mail girildi.");
         //5) Enter incorrect password in password textbox.
         ReusableMethods.clickWithJS(homePage.passwordBox);
         homePage.passwordBox.sendKeys("wrongPassword");
+        extentTest.info("yanlis password girildi.");
         //6) Click on login button
         homePage.loginButton.click();
+        extentTest.info("Giriş düğmesine tıklandi.");
         //7) Proper error must be displayed(ie Invalid username) and prompt to enter login again
         assert homePage.wrongText.isDisplayed();
+        extentTest.info("Hata mesaji goruntulendi.");
         Driver.getDriver().navigate().refresh();
+        extentTest.pass("PASS");
     }
 
     @AfterClass
