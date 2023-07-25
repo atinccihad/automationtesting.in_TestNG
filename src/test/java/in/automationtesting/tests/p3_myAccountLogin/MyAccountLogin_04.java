9d77e572db6530dbe5d9827056940ecee5518369
atinccihad <atinccihad@gmail.com>
2023-07-25 17:12:45 +0300

    automationtesting 0.22

---
+++ src/test/java/in/automationtesting/tests/p3_myAccountLogin/MyAccountLogin_04.java
@@ -0,0 +1,47 @@
+package in.automationtesting.tests.p3_myAccountLogin;
+
+import in.automationtesting.pages.HomePage;
+import in.automationtesting.utilities.ConfigurationReader;
+import in.automationtesting.utilities.Driver;
+import in.automationtesting.utilities.ReusableMethods;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class MyAccountLogin_04 {
+    //22. Log-in with empty username and valid password
+    HomePage homePage = new HomePage();
+
+    @BeforeClass
+    public void getUrl_login() {
+        //1) Open the browser
+        //2) Enter the URL “http://practice.automationtesting.in/”
+        Driver.getDriver().get(ConfigurationReader.getProperty("base_url"));
+    }
+
+    @Test
+    public void myAccountLogin_03() {
+        //3) Click on My Account Menu
+        Driver.getDriver().navigate().refresh();
+        ReusableMethods.clickWithJS(homePage.myAccountButton);
+        homePage.myAccountButton.click();
+        //4) Enter empty username in username textbox
+        Driver.getDriver().navigate().refresh();
+        homePage.myAccountButton.click();
+        homePage.emailBox.sendKeys("", Keys.ENTER);
+        //5) Now enter valid password in the password textbox
+        ReusableMethods.clickWithJS(homePage.passwordBox);
+        homePage.passwordBox.sendKeys(ConfigurationReader.getProperty("password"));
+        //6) Click on login button
+        homePage.loginButton.click();
+        //7) Proper error must be displayed(ie Invalid username) and prompt to enter login again
+        assert homePage.wrongUsernameInfo.isDisplayed();
+        Driver.getDriver().navigate().refresh();
+    }
+
+    @AfterClass
+    public void closeDriver() {Driver.closeDriver();}
+
+}
+
