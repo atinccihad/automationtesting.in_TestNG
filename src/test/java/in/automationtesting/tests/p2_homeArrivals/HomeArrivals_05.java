7f865594f938db05e467abd80df2379b5971e8bf
atinccihad <atinccihad@gmail.com>
2023-07-26 14:21:24 +0300

    automationtesting 0.12.1

--- src/test/java/in/automationtesting/tests/p2_homeArrivals/HomeArrivals_05.java
+++ src/test/java/in/automationtesting/tests/p2_homeArrivals/HomeArrivals_05.java
@@ -4,6 +4,7 @@ import in.automationtesting.pages.HomePage;
 import in.automationtesting.utilities.ConfigurationReader;
 import in.automationtesting.utilities.Driver;
 import in.automationtesting.utilities.ReusableMethods;
+import in.automationtesting.utilities.TestBaseRapor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterClass;
@@ -11,7 +12,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-public class HomeArrivals_06 {
+public class HomeArrivals_05 extends TestBaseRapor {
     //12. Home-Arrivals-Add to Basket-Items-Add book
     HomePage homePage = new HomePage();
     Actions actions = new Actions(Driver.getDriver());
@@ -27,14 +28,17 @@ public class HomeArrivals_06 {
 
     @Test
     public void homeArrivals_05() {
+        extentTest = extentReports.createTest("HomeArrivals_05", "Home-Arrivals-Add to Basket-Items-Add book.");
         ///3) Click on Shop Menu
         homePage.shopMenuButton.click();
         Driver.getDriver().navigate().refresh();
         ReusableMethods.waitFor(2);
         homePage.shopMenuButton.click();
+        extentTest.info("shopMenuButton is clicked.");
 
         //4) Now click on Home menu button
         ReusableMethods.clickWithJS(homePage.homeMenuButton);
+        extentTest.info("homeMenuButton is clicked.");
         Driver.getDriver().navigate().refresh();
         ReusableMethods.waitFor(2);
 
@@ -43,6 +47,7 @@ public class HomeArrivals_06 {
         Driver.getDriver().navigate().refresh();
         actions.sendKeys(Keys.PAGE_DOWN).sendKeys(Keys.PAGE_DOWN).perform();
         softAssert.assertEquals(homePage.arrivals.size(), 3, "Expected value three arrivals, actual value " + homePage.arrivals.size());
+        extentTest.info("The Home page must contains only three arrivals.");
 
         //7) Now click the image in the Arrivals
         for (int i = 0; i < homePage.arrivals.size(); i++) {
@@ -54,41 +59,48 @@ public class HomeArrivals_06 {
         ReusableMethods.clickWithJS(homePage.arrivals.get(0));
         Driver.getDriver().navigate().refresh();
         softAssert.assertTrue(homePage.addToBasketButton.isDisplayed(), "homePage.addToBasketButton.is not Displayed !!");
+        extentTest.info("addToBasketButton.is Displayed.");
 
         //9) Image should be clickable and shoul navigate to next page where user can add that book to his basket
         softAssert.assertTrue(homePage.firstArrivalImage.isEnabled(), "Image not should be clickable!!");
+        extentTest.info("firstArrivalImage is should be clickable.");
         ReusableMethods.clickWithJS(homePage.firstArrivalImage);
         softAssert.assertTrue(homePage.addToBasketButton.isEnabled(), "homePage.addToBasketButton.is not Selected!!");
+        extentTest.info("addToBasketButton is should be Selected.");
         Driver.getDriver().navigate().refresh();
 
         //10) Click on the Add To Basket button which adds that book to your basket
         ReusableMethods.clickWithJS(homePage.addToBasketButton);
+        extentTest.info("addToBasketButton clicked.");
 
         //11) User can view that Book in the Menu item with price.
         softAssert.assertTrue(homePage.bookImage.isDisplayed(), "homePage.bookImage.is not Displayed!!");
+        extentTest.info("bookImage is Displayed.");
         softAssert.assertTrue(homePage.price.isDisplayed(), "homePage.price.is not Displayed!!");
+        extentTest.info("price is Displayed.");
 
         //12) Now click on Item link which navigates to proceed to check out page.
         //13) Click on textbox value under quantity in Check out page to add or subtract books.
         homePage.itemsAndPriceLinkButton.click();
+        extentTest.info("navigates to proceed to check out page.");
 
         //14) Now after the above change ‘Update Basket’ button will turn into Clickable mode.
         homePage.quantityBox.sendKeys("2");
         softAssert.assertTrue(homePage.updateBasketButton.isSelected());
+        extentTest.info("updateBasketButton is Selected.");
 
         //15) Now click on Update Basket to reflect those changes
         ReusableMethods.clickWithJS(homePage.updateBasketButton);
+        extentTest.info("updateBasketButton is clicked.");
         ReusableMethods.clickWithJS(homePage.proceedToCheckoutButton);
+        extentTest.info("proceedToCheckoutButton is clicked.");
 
         //16) User has the feasibility to Update Basket at the time of check out.
         ReusableMethods.jsExecutorScrool(homePage.yourOrderUrunMiktarElementi);
         softAssert.assertTrue(homePage.yourOrderUrunMiktarElementi.isSelected(), "Urun miktari odeme sayfasinda degistirilemedi!!");
-
+        extentTest.info("Urun miktari odeme sayfasinda degistirilebilmeli.");
+        extentTest.pass("Urun miktari odeme sayfasinda degistirildi.");
         softAssert.assertAll();
     }
 
-    @AfterClass
-    public void closeDriver() {
-        Driver.getDriver().close();
-    }
 }
