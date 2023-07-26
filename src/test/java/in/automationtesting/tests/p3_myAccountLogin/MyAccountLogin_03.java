0a545b23f09aaf825fb997fe916e81f90f363dc6
atinccihad <atinccihad@gmail.com>
2023-07-26 16:51:27 +0300

    automationtesting 0.19.1

--- src/test/java/in/automationtesting/tests/p3_myAccountLogin/MyAccountLogin_03.java
+++ src/test/java/in/automationtesting/tests/p3_myAccountLogin/MyAccountLogin_03.java
@@ -9,9 +9,10 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class MyAccountLogin_03  extends TestBaseRapor {
+public class MyAccountLogin_03 extends TestBaseRapor {
     //21. Log-in with correct username and empty password
     HomePage homePage = new HomePage();
+
     @BeforeClass
     public void getUrl_login() {
         //1) Open the browser
@@ -21,32 +22,34 @@ public class MyAccountLogin_03  extends TestBaseRapor {
 
     @Test
     public void myAccountLogin_03() {
-        extentTest = extentReports.createTest("myAccountLogin_08","Gecerli username ve yanlis sifre ile giris yapilamamali.");
+        extentTest = extentReports.createTest("myAccountLogin_08", "Gecerli username ve yanlis sifre ile giris yapilamamali.");
+
         //3) Click on My Account Menu
         Driver.getDriver().navigate().refresh();
         ReusableMethods.clickWithJS(homePage.myAccountButton);
         homePage.myAccountButton.click();
+
         //4) Enter incorrect username in username textbox
         Driver.getDriver().navigate().refresh();
         homePage.myAccountButton.click();
         extentTest.info("myAccountButton is clicked.");
         homePage.emailBox.sendKeys(ConfigurationReader.getProperty("username"));
         extentTest.info("Kullanıcı adı metin kutusuna durum değiştirilen kullanıcı adı girildi.");
+
         //5) Enter incorrect password in password textbox.
         ReusableMethods.clickWithJS(homePage.passwordBox);
         homePage.passwordBox.sendKeys("wrongPassword");
         extentTest.info("Parola metin kutusuna wrongPassword girildi.");
+
         //6) Click on login button
         homePage.loginButton.click();
         extentTest.info("Giriş düğmesine tıklandi.");
+
         //7) Proper error must be displayed(ie Invalid username) and prompt to enter login again
         assert homePage.wrongText.isDisplayed();
         extentTest.info("wrongText goruntulendi.");
         Driver.getDriver().navigate().refresh();
         extentTest.pass("PASS");
     }
-
-    @AfterClass
-    public void closeDriver(){Driver.closeDriver();}
 
 }
