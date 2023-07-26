a27efbe9bc05df7e08724cf2ac7af52611a11fe7
atinccihad <atinccihad@gmail.com>
2023-07-26 07:03:40 +0300

    automationtesting 0.2.1

--- src/test/java/in/automationtesting/tests/p1_homePage/HomePage_02.java
+++ src/test/java/in/automationtesting/tests/p1_homePage/HomePage_02.java
@@ -4,6 +4,7 @@ import in.automationtesting.pages.HomePage;
 import in.automationtesting.utilities.ConfigurationReader;
 import in.automationtesting.utilities.Driver;
 import in.automationtesting.utilities.ReusableMethods;
+import in.automationtesting.utilities.TestBaseRapor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterClass;
@@ -12,7 +13,7 @@ import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
-public class HomePage_02 {
+public class HomePage_02 extends TestBaseRapor {
     //2. Home page with three Arrivals only
     HomePage homePage = new HomePage();
     Actions actions = new Actions(Driver.getDriver());
@@ -26,6 +27,7 @@ public class HomePage_02 {
 
     @Test
     public void homePage_02() throws InterruptedException {
+        extentTest = extentReports.createTest("homePage_02", "Home Page contains with three Arrivals.");
         homePage.shopMenuButton.click();
         Driver.getDriver().navigate().refresh();
 
@@ -34,22 +36,24 @@ public class HomePage_02 {
         Driver.getDriver().navigate().refresh();
         ReusableMethods.waitFor(2);
         homePage.shopMenuButton.click();
+        extentTest.info("shopMenuButton is clicked.");
 
         //4) Now click on Home menu button
         homePage.homeMenuButton.click();
         Driver.getDriver().navigate().refresh();
         ReusableMethods.waitFor(2);
+        extentTest.info("homeMenuButton is clicked.");
 
         //5) Test whether the Home page has Three Arrivals only
         //6) The Home page must contains only three Arrivals
         Driver.getDriver().navigate().refresh();
         actions.sendKeys(Keys.PAGE_DOWN).sendKeys(Keys.PAGE_DOWN).perform();
         assertEquals(homePage.arrivals.size(), 3, "Expected value three arrivals, actual value " + homePage.arrivals.size());
+        extentTest.info("The Home page contains three Arrivals.");
+        extentTest.pass("The homepage contains only three Arrivals.");
     }
 
     @AfterClass
-    public void closeDriver() {
-        Driver.getDriver().close();
-    }
+    public void closeDriver() {Driver.getDriver().close();}
 
 }
