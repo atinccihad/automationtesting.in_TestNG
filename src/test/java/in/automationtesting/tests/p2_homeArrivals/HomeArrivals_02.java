c39bd9ea179fd6bc490b0bc769c959bc293b9fcc
atinccihad <atinccihad@gmail.com>
2023-07-26 13:16:21 +0300

    automationtesting 0.9.1

--- src/test/java/in/automationtesting/tests/p2_homeArrivals/HomeArrivals_02.java
+++ src/test/java/in/automationtesting/tests/p2_homeArrivals/HomeArrivals_02.java
@@ -13,7 +13,7 @@ import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 public class HomeArrivals_02 extends TestBaseRapor {
-    //8. Home-Arrivals-Add to Basket-Items
+    //9. Home-Arrivals-Add to Basket-Items-Coupon
     HomePage homePage = new HomePage();
     Actions actions = new Actions(Driver.getDriver());
     SoftAssert softAssert = new SoftAssert();
@@ -27,15 +27,17 @@ public class HomeArrivals_02 extends TestBaseRapor {
 
     @Test
     public void homeArrivals_02() {
-
+        extentTest = extentReports.createTest("HomeArrivals_02", "Arrivals-Add to Basket-Items-Coupon.");
         ///3) Click on Shop Menu
         homePage.shopMenuButton.click();
         Driver.getDriver().navigate().refresh();
         ReusableMethods.waitFor(2);
         homePage.shopMenuButton.click();
+        extentTest.info("shopMenuButton is clicked.");
 
         //4) Now click on Home menu button
         homePage.homeMenuButton.click();
+        extentTest.info("homeMenuButton is clicked.");
         Driver.getDriver().navigate().refresh();
         ReusableMethods.waitFor(2);
 
@@ -44,6 +46,7 @@ public class HomeArrivals_02 extends TestBaseRapor {
         Driver.getDriver().navigate().refresh();
         actions.sendKeys(Keys.PAGE_DOWN).sendKeys(Keys.PAGE_DOWN).perform();
         softAssert.assertEquals(homePage.arrivals.size(), 3, "Expected value three arrivals, actual value " + homePage.arrivals.size());
+        extentTest.info("The Home page must contains only three sliders.");
 
         //7) Now click the image in the Arrivals
         for (int i = 0; i < homePage.arrivals.size(); i++) {
@@ -54,27 +57,43 @@ public class HomeArrivals_02 extends TestBaseRapor {
         //8) Test whether it is navigating to next page where the user can add that book into his basket.
         ReusableMethods.clickWithJS(homePage.arrivals.get(0));
         softAssert.assertTrue(homePage.addToBasketButton.isDisplayed(), "homePage.addToBasketButton.is not Displayed !!");
+        extentTest.info("addToBasketButton.is Displayed.");
 
         //9) Image should be clickable and shoul navigate to next page where user can add that book to his basket
         softAssert.assertTrue(homePage.firstArrivalImage.isEnabled(), "Image not should be clickable!!");
+        extentTest.info("firstArrivalImage is should be clickable.");
         ReusableMethods.clickWithJS(homePage.firstArrivalImage);
         softAssert.assertTrue(homePage.addToBasketButton.isEnabled(), "homePage.addToBasketButton.is not Selected!!");
+        extentTest.info("addToBasketButton is should be Selected.");
         Driver.getDriver().navigate().refresh();
 
         //10) Click on the Add To Basket button which adds that book to your basket
         ReusableMethods.clickWithJS(homePage.addToBasketButton);
+        extentTest.info("addToBasketButton clicked.");
 
         //11) User can view that Book in the Menu item with price.
         softAssert.assertTrue(homePage.bookImage.isDisplayed(), "homePage.bookImage.is not Displayed!!");
+        extentTest.info("bookImage is Displayed.");
         softAssert.assertTrue(homePage.price.isDisplayed(), "homePage.price.is not Displayed!!");
+        extentTest.info("price is Displayed.");
 
         //12) Now click on Item link which navigates to proceed to check out page.
         homePage.itemsAndPriceLinkButton.click();
+        extentTest.info("LinkButton clicked.");
 
         //13) User can click on the Item link in menu item after adding the book in to the basket which leads to the check out page
         softAssert.assertTrue(homePage.proceedToCheckoutButton.isEnabled(), "proceedToCheckoutButton is not Selected!!");
+        extentTest.info("proceedToCheckoutButton is Enabled.");
+
+        //14) Enter the Coupon code as ‘krishnasakinala’ to get 50rps off on the total.
+        homePage.couponCodeBox.sendKeys(homePage.couponCode, Keys.ENTER);
+        extentTest.info("send the Coupon code, Received 50rps discount from total price..");
+
+        //15) User can able to apply coupon by entering ‘krishnasakinala’ in the coupon textbox which give 50rps off on the total price
+        softAssert.assertNotEquals(homePage.indirimliTotal.getText(), homePage.price.getText(), "Indirim uygulanmamasi!!");
+        extentTest.info("The discount has been confirmed.");
+        extentTest.pass("Add to Basket-Items-Coupon test PASS.");
         softAssert.assertAll();
     }
-
 
 }
