79fff8d022cf8f7562339cdec3aa2bdc55d425cc
atinccihad <atinccihad@gmail.com>
2023-07-26 16:12:49 +0300

    automationtesting 0.16.1

--- src/test/java/in/automationtesting/tests/p2_homeArrivals/HomeArrivals_9.java
+++ src/test/java/in/automationtesting/tests/p2_homeArrivals/HomeArrivals_9.java
@@ -12,7 +12,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-public class HomeArrivals_10 extends TestBaseRapor {
+public class HomeArrivals_9 extends TestBaseRapor {
     //16. Home-Arrivals-Add to Basket-Items-Check-out functionality
     HomePage homePage = new HomePage();
     Actions actions = new Actions(Driver.getDriver());
@@ -27,23 +27,28 @@ public class HomeArrivals_10 extends TestBaseRapor {
     }
 
     @Test
-    public void homeArrivals_05() {
-        extentTest = extentReports.createTest("myAccountLogin_08","Gecerli username ve sifre ile giris yapabilmeli.");
+    public void homeArrivals_09() {
+        extentTest = extentReports.createTest("myAccountLogin_08", "Gecerli username ve sifre ile giris yapabilmeli.");
         ///3) Click on Shop Menu
         homePage.shopMenuButton.click();
         Driver.getDriver().navigate().refresh();
         ReusableMethods.waitFor(2);
         homePage.shopMenuButton.click();
+        extentTest.info("shopMenuButton is clicked.");
+
         //4) Now click on Home menu button
         ReusableMethods.clickWithJS(homePage.homeMenuButton);
         extentTest.info("homeMenuButton is clicked.");
         Driver.getDriver().navigate().refresh();
         ReusableMethods.waitFor(2);
+
         //5) Test whether the Home page has Three Arrivals only
         //6) The Home page must contains only three Arrivals
         Driver.getDriver().navigate().refresh();
         actions.sendKeys(Keys.PAGE_DOWN).sendKeys(Keys.PAGE_DOWN).perform();
         softAssert.assertEquals(homePage.arrivals.size(), 3, "Expected value three arrivals, actual value " + homePage.arrivals.size());
+        extentTest.info("The Home page must contains only three arrivals.");
+
         //7) Now click the image in the Arrivals
         for (int i = 0; i < homePage.arrivals.size(); i++) {
             ReusableMethods.clickWithJS(homePage.arrivals.get(i));
@@ -56,6 +61,7 @@ public class HomeArrivals_10 extends TestBaseRapor {
         Driver.getDriver().navigate().refresh();
         softAssert.assertTrue(homePage.addToBasketButton.isDisplayed(), "homePage.addToBasketButton.is not Displayed !!");
         extentTest.info("addToBasketButton is displayed.");
+
         //9) Image should be clickable and shoul navigate to next page where user can add that book to his basket
         softAssert.assertTrue(homePage.firstArrivalImage.isEnabled(), "Image not should be clickable!!");
         ReusableMethods.clickWithJS(homePage.firstArrivalImage);
@@ -64,18 +70,25 @@ public class HomeArrivals_10 extends TestBaseRapor {
 
         //10) Click on the Add To Basket button which adds that book to your basket
         ReusableMethods.clickWithJS(homePage.addToBasketButton);
+        extentTest.info("addToBasketButton clicked.");
 
         //11) User can view that Book in the Menu item with price.
         softAssert.assertTrue(homePage.bookImage.isDisplayed(), "homePage.bookImage.is not Displayed!!");
+        extentTest.info("bookImage is Displayed.");
         softAssert.assertTrue(homePage.price.isDisplayed(), "homePage.price.is not Displayed!!");
+        extentTest.info("price is Displayed.");
 
         //12) Now click on Item link which navigates to proceed to check out page.
         ReusableMethods.clickWithJS(homePage.viewBasketButton);
+        extentTest.info("viewBasketButton is clicked.");
 
         //13) Now user can find total and subtotal values just above the Proceed to Checkout button.
         softAssert.assertTrue(homePage.subtotal.isDisplayed(), "subtotal is not displayed!!");
+        extentTest.info("subtotal is displayed.");
         softAssert.assertTrue(homePage.tax.isDisplayed(), "tax is not displayed!!");
+        extentTest.info("tax is displayed.");
         softAssert.assertTrue(homePage.total.isDisplayed(), "total is not displayed!!");
+        extentTest.info("total is displayed.");
 
         //14) The total always < subtotal because taxes are added in the subtotal
         String totalStr = homePage.total.getText();
@@ -101,14 +114,12 @@ public class HomeArrivals_10 extends TestBaseRapor {
         //16) Clicking on Proceed to Checkout button leads to payment gateway page
         //17)Clicking on Proceed to Checkout button leads to payment gateway page
         ReusableMethods.clickWithJS(homePage.proceedToCheckoutButton);
+        extentTest.info("proceedToCheckoutButton clicked.");
         softAssert.assertEquals(Driver.getDriver().getCurrentUrl(), homePage.checkoutUrl, "Checkout page'e ulasilamadi!!");
+        extentTest.info("Checkout page'e ulasildi.");
+        extentTest.pass("Fonksiyonlar test edildi.");
 
         softAssert.assertAll();
-    }
-
-    @AfterClass
-    public void closeDriver() {
-        Driver.getDriver().close();
     }
 
 }
