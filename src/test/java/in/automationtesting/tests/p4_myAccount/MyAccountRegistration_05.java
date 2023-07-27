2030a3cfd05e373d034974e9ed809a9649c25353
atinccihad <atinccihad@gmail.com>
2023-07-27 19:29:11 +0300

    automationtesting 0.28.2

--- src/test/java/in/automationtesting/tests/p4_myAccount/MyAccountRegistration_05.java
+++ src/test/java/in/automationtesting/tests/p4_myAccount/MyAccountRegistration_05.java
@@ -5,11 +5,15 @@ import in.automationtesting.utilities.ConfigurationReader;
 import in.automationtesting.utilities.Driver;
 import in.automationtesting.utilities.ReusableMethods;
 import in.automationtesting.utilities.TestBaseRapor;
+import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertTrue;
+
 public class MyAccountRegistration_05 extends TestBaseRapor {
-    //23. Log-in with empty username and empty password
+    //31. Registration with empty Email-id & password
     RegistrationPage registrationPage = new RegistrationPage();
+
     @Test
     public void myAccountRegistration_05() {
         extentTest = extentReports.createTest("MyAccountRegistration_05", "Bos kullanici adi, bos password ile kayit gerceklesmemeli.");
@@ -23,6 +27,25 @@ public class MyAccountRegistration_05 extends TestBaseRapor {
         ReusableMethods.waitFor(3);
         ReusableMethods.clickWithJS(registrationPage.myAccountButton);
         extentTest.info("myAccountButton clicked.");
+
+        //4) Enter empty Email Address in Email-Address textbox
+        Driver.getDriver().navigate().refresh();
+        ReusableMethods.clickWithJS(registrationPage.myAccountButton);
+        registrationPage.emailBox.sendKeys("", Keys.ENTER);
+        extentTest.info("Entered empty Email Address in Email-Address textbox.");
+
+        //5) Enter empty password in password textbox
+        registrationPage.passwordBox.sendKeys("");
+        extentTest.info("The empty password has been entered in the password text box.");
+
+        //6) Click on Register button
+        registrationPage.registerButton.click();
+        extentTest.info("RegisterButton clicked.");
+
+        //7) Registration must fail with a warning message(ie please provide valid email address)
+        assertTrue(registrationPage.errorMessage.isDisplayed(), "Warning message is not displayed.");
+        extentTest.info("Registration is fail with a warning message.");
+        extentTest.pass("Bos kullanici adi, bos password ile kayit gerceklesmedi.");
     }
 
 }
