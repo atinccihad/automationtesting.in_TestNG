74892b1e34647a77b1fe5ea7d0d920cb33c84170
atinccihad <atinccihad@gmail.com>
2023-07-27 15:08:56 +0300

    automationtesting 0.26

--- src/test/java/in/automationtesting/pages/RegistrationPage.java
+++ src/test/java/in/automationtesting/pages/RegistrationPage.java
@@ -7,9 +7,10 @@ import org.openqa.selenium.support.PageFactory;
 
 public class RegistrationPage {
     public RegistrationPage(){ PageFactory.initElements(Driver.getDriver(),this); }
-    @FindBy(id = "menu-item-50") public WebElement myAccountButton;
+    @FindBy(xpath = "//a[text()='My Account']") public WebElement myAccountButton;
     @FindBy(css = "#username") public WebElement emailBox;
     @FindBy(css = "#password") public WebElement passwordBox;
     @FindBy(xpath = "//input[@class='woocommerce-Button button']") public WebElement registerButton;
     @FindBy(xpath = "(//div/p)[1]") public WebElement helloUser;
+    @FindBy(xpath = "//li[*='invalidMail']") public WebElement emailWarningMessage;
 }
---
+++ src/test/java/in/automationtesting/tests/p4_myAccount/MyAccountRegistration_02.java
@@ -0,0 +1,49 @@
+package in.automationtesting.tests.p4_myAccount;
+
+import in.automationtesting.pages.RegistrationPage;
+import in.automationtesting.utilities.ConfigurationReader;
+import in.automationtesting.utilities.Driver;
+import in.automationtesting.utilities.ReusableMethods;
+import in.automationtesting.utilities.TestBaseRapor;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class MyAccountRegistration_02 extends TestBaseRapor {
+    //28. Registration with invalid Email-id
+    RegistrationPage registrationPage = new RegistrationPage();
+    Actions actions = new Actions(Driver.getDriver());
+
+    @Test
+    public void registrationWithInvalidEmailId() {
+        extentTest = extentReports.createTest("MyAccountRegistration_02", "Gecersiz email-id ile kayit gerceklesmemeli.");
+        //1) Open the browser
+        //2) Enter the URL “http://practice.automationtesting.in/”
+        Driver.getDriver().get(ConfigurationReader.getProperty("base_url"));
+        extentTest.info("Get the url -> http://practice.automationtesting.in/");
+        //3) Click on My Account Menu
+        ReusableMethods.waitFor(3);
+        ReusableMethods.clickWithJS(registrationPage.myAccountButton);
+        extentTest.info("myAccountButton clicked.");
+
+        //4) Enter invalid Email Address in Email-Address textbox
+        Driver.getDriver().navigate().refresh();
+        registrationPage.emailBox.sendKeys(ConfigurationReader.getProperty("invalidUsername"));
+        extentTest.info("Enter the invalid Email Address.");
+
+        //5) Enter your own password in password textbox
+        registrationPage.passwordBox.sendKeys(ConfigurationReader.getProperty("password"));
+        extentTest.info("The password has been entered in the password text box.");
+
+        //6) Click on Register button
+        registrationPage.registerButton.click();
+        extentTest.info("The registerButton clicked.");
+
+        //7) Registration must fail with a warning message(ie You must enter a valid email address)
+        assertTrue(registrationPage.emailWarningMessage.isDisplayed(), "The password you entered for the username invalidMail is incorrect. Lost your password? mesaji goruntulenmedi!");
+        extentTest.info("invalidMail  assertion.");
+        extentTest.pass("Registration with invalid email id test PASS.");
+    }
+
+}
