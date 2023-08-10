ca7fe3f8bebd04e88c24d4183e6cd135e37b1c40
atinccihad <atinccihad@gmail.com>
2023-08-11 00:17:46 +0300

    automationtesting 0.39.0

--- src/test/java/in/automationtesting/tests/p5_shop/Shop_10.java
+++ src/test/java/in/automationtesting/tests/p5_shop/Shop_10.java
@@ -1,16 +1,23 @@
 package in.automationtesting.tests.p5_shop;
 
+import in.automationtesting.pages.HomePage;
 import in.automationtesting.pages.ShopPage;
 import in.automationtesting.utilities.ConfigurationReader;
 import in.automationtesting.utilities.Driver;
+import in.automationtesting.utilities.ReusableMethods;
 import in.automationtesting.utilities.TestBaseRapor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
 
 public class Shop_10 extends TestBaseRapor {
     //41. Shop-Add to Basket-View Basket Functionality
     ShopPage shopPage = new ShopPage();
     Actions actions = new Actions(Driver.getDriver());
+    HomePage homepage = new HomePage();
 
     @Test
     public void shopDefaultSortingFunctionality() {
@@ -21,23 +28,55 @@ public class Shop_10 extends TestBaseRapor {
         extentTest.info("Get the url -> http://practice.automationtesting.in/");
 
         //3) Click on Shop Menu
-        shopPage.shopButton.click();
+        homepage.shopBtn.click();
         Driver.getDriver().navigate().refresh();
-        shopPage.shopButton.click();
-        extentTest.info("shopButton tiklandi.");
+        ReusableMethods.waitAndClick(homepage.shopBtn, 3);
+        extentTest.info("Clicked Shop Menu");
 
         //4) Click on the Add To Basket button which adds that book to your basket
-        //5) User can view that Book in the Menu item with price .
-        //6) Now click on View Basket link which navigates to proceed to check out page.
-        //7) Now user can find total and subtotal values just above the Proceed to Checkout button.
-        //8) The total always < subtotal because taxes are added in the subtotal
-        //9) Now click on Proceed to Check out button which navigates to payment gateway page.
-        //10) User can view Billing Details,Order Details,Additional details and Payment gateway details.
-        //11) Now user can fill his details in billing details form and can opt any payment in the payment gateway like Direct bank transfer,cheque,cash or paypal.
-        //12) Now click on Place Order button to complete process
-        //13) On clicking place order button user completes his process where the page navigates to Order confirmation page with order details,bank details,customer details and billing details.
+        ReusableMethods.clickWithJS(shopPage.shopPageAddToBasketBtn);
+        extentTest.info("Clicked shopPageAddToBasketBtn");
 
-        extentTest.pass("");
+        //5) User can view that Book in the Menu item with price .
+        String itemText = homepage.itemText.getText();
+        assertEquals(itemText, "1 Item");
+        extentTest.info("1 Item  assertion");
+        String priceText = homepage.priceText.getText();
+        assertEquals(priceText, "₹500.00");
+        extentTest.info("priceText  assertion");
+
+        //6) Now click on Item link which navigates to proceed to check out page.
+        ReusableMethods.clickWithJS(homepage.viewBasketButton);
+        extentTest.info("clicked viewBasketButton");
+
+        //7) Now user can find total and subtotal values just above the Proceed to Checkout button.
+        Driver.getDriver().navigate().refresh();
+        assertTrue(homepage.subtotalValue.isDisplayed() && homepage.finalTotalPrice.isDisplayed());
+        extentTest.info("subtotalValue isDisplayed");
+
+        //8) The total always < subtotal because taxes are added in the subtotal
+        double subtotal = Double.parseDouble(homepage.subtotalValue.getText().substring(1));
+        double totalPrice = Double.parseDouble(homepage.finalTotalPrice.getText().substring(1));
+        assertTrue(totalPrice > subtotal);
+        extentTest.info("totalPrice > subtotal");
+
+        //9) The tax rate variers for India compared to other countries
+        ReusableMethods.clickWithJS(shopPage.checkoutBtn);
+        extentTest.info("checkoutBtn clicked");
+        double taxForIndia = Double.parseDouble(shopPage.taxForIndia.getText().substring(1));
+        double totalAtLastPage = Double.parseDouble(shopPage.totalAtLastPage.getText().substring(1));
+        assertEquals((totalAtLastPage * 2) / 100, taxForIndia);
+        extentTest.info("totalAtLastPage * 2) / 100, taxForIndia -> EQUAL");
+
+        //10) Tax rate for indian should be 2% and for abroad it should be 5%
+        shopPage.billingCountryDropdown.click();
+        shopPage.billingCountryInputBox.sendKeys("Turkey");
+        shopPage.billingCountryInputBox.sendKeys(Keys.ENTER);
+        ReusableMethods.wait(10);
+        double taxForotherCountries = Double.parseDouble(shopPage.taxForOtherCountries.getText().substring(1));
+        assertEquals((totalAtLastPage * 5) / 100, taxForotherCountries);
+        extentTest.info("totalAtLastPage * 5) / 100, taxForotherCountries -> EQUAL");
+        extentTest.pass("Shop-Add to Basket-View Basket Functionality test PASS.");
     }
 
 }
