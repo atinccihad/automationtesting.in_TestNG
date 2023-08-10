d0b275a330dc71ccb99ee721f69dc1e0f63828ef
atinccihad <atinccihad@gmail.com>
2023-08-11 00:53:30 +0300

    shopPage 0.3

--- src/test/java/in/automationtesting/pages/ShopPage.java
+++ src/test/java/in/automationtesting/pages/ShopPage.java
@@ -65,113 +65,21 @@ public class ShopPage {
     @FindBy(xpath = "//span[@id='select2-chosen-2']") public WebElement statecountryKapali;
     @FindBy(xpath = "//input[@id=\"billing_postcode\"]") public WebElement postcodeZIP;
     @FindBy(xpath = "/html/body/div[1]/div[2]/div/div/div/div/div[1]/p[1]") public WebElement thanksMessage;
-
-
-
-    @FindBy(xpath = "//span[@style='left: 100%;']")
-    public WebElement adjustRightBtn;
-
-    @FindBy(xpath = "//span[@style='left: 85.7143%;']")
-    public WebElement adjustLeft450;
-
-    @FindBy(xpath = "//button[.='Filter']")
-    public WebElement filterBtn;
-
-    @FindBy(xpath = "//ul[@class='product-categories']/li")
-    public List<WebElement> productList;
-
-    @FindBy(xpath = "//a[.='Android']")
-    public WebElement androidBtn;
-
-    @FindBy(xpath = "//nav[@class='woocommerce-breadcrumb']/a")
-    public WebElement productPage;
-
-    @FindBy(tagName = "h3")
-    public List<WebElement> productTitle;
-
-    @FindBy(xpath = "//select[@name='orderby']")
-    public WebElement sortDropdown;
-
-    @FindBy(xpath = "//a[@data-product_id='160']")
-    public WebElement shopPageAddToBasketBtn;
-
-    @FindBy(xpath = "//*[.='View Basket']")
-    public WebElement viewBasketBtn;
-
-    @FindBy(xpath = "//a[@class='checkout-button button alt wc-forward']")
-    public WebElement checkoutBtn;
-
-    @FindBy(xpath = "//h3[.='Billing Details']")
-    public WebElement billingDetails;
-
-    @FindBy(xpath = "//h3[.='Additional Information']")
-    public WebElement additionalInformation;
-    @FindBy(xpath = "//h3[.='Your order']")
-    public WebElement peymentGateWay;
-
-    @FindBy(id = "billing_first_name")
-    public WebElement billingFirsNameInputBox;
-
-    @FindBy(id = "billing_last_name")
-    public WebElement billingLastNameInputBox;
-
-    @FindBy(id = "billing_email")
-    public WebElement billing_emailInputBox;
-
-    @FindBy(id = "billing_phone")
-    public WebElement billing_phone_InputBox;
-    @FindBy(id = "s2id_billing_country")
-    public WebElement billingCountryDropdown;
-    @FindBy(xpath = "//li[@class='select2-results-dept-0 select2-result select2-result-selectable']")
-    public List<WebElement> countryOptions;
-    @FindBy(id = "s2id_autogen1_search")
-    public WebElement billingCountryInputBox;
-
-    @FindBy(id = "billing_address_1")
-    public WebElement billingAddress;
-
-    @FindBy(id = "billing_postcode")
-    public WebElement billingPostCodeInputBox;
-
-    @FindBy(id = "billing_state")
-    public WebElement billingState;
-
-    @FindBy(xpath = "//li[@class='select2-results-dept-0 select2-result select2-result-selectable select2-highlighted']")
-    public WebElement billingStateOption;
-
-    @FindBy(id = "billing_city")
-    public WebElement billing_cityInputBox;
-
-    @FindBy(id = "select2-chosen-2")
-    public WebElement billingProvincedropdown;
-
-    @FindBy(xpath = "//li[@class='select2-results-dept-0 select2-result select2-result-selectable']")
-    public List<WebElement> billingProvinceOptions;
-    @FindBy(id = "s2id_autogen2_search")
-    public WebElement billingProvinceInputBox;
-
-    @FindBy(id = "place_order")
-    public WebElement placeOrderBtn;
-    @FindBy(xpath = "//p[@class='woocommerce-thankyou-order-received']")
-    public WebElement orderConfirmMessage;
-    @FindBy(xpath = "//h2[.='Order Details']")
-    public WebElement orderDetails;
-
-    @FindBy(xpath = "//h2[.='Our Bank Details']")
-    public WebElement bankDetails;
-
-    @FindBy(xpath = "//tr[@class='tax-rate tax-rate-in-tax-1']/td")
-    public WebElement taxForIndia;
-
-    @FindBy(xpath = "//tr[@class='tax-rate tax-rate-roaming-tax-1']/td")
-    public WebElement taxForOtherCountries;
-
-    @FindBy(xpath = "//td[@class='product-total']/span")
-    public WebElement totalAtLastPage;
-
-    @FindBy(xpath = "//span[@tabindex='0']")
-    public WebElement rightSliderHandleIcon;
-
-
+    @FindBy(xpath = "//span[@style='left: 100%;']") public WebElement adjustRightBtn;
+    @FindBy(xpath = "//span[@style='left: 85.7143%;']") public WebElement adjustLeft450;
+    @FindBy(xpath = "//button[.='Filter']") public WebElement filterBtn;
+    @FindBy(xpath = "//ul[@class='product-categories']/li") public List<WebElement> productList;
+    @FindBy(xpath = "//a[.='Android']") public WebElement androidBtn;
+    @FindBy(xpath = "//nav[@class='woocommerce-breadcrumb']/a") public WebElement productPage;
+    @FindBy(tagName = "h3") public List<WebElement> productTitle;
+    @FindBy(xpath = "//select[@name='orderby']") public WebElement sortDropdown;
+    @FindBy(xpath = "//a[@data-product_id='160']") public WebElement shopPageAddToBasketBtn;
+    @FindBy(xpath = "//*[.='View Basket']") public WebElement viewBasketBtn;
+    @FindBy(xpath = "//a[@class='checkout-button button alt wc-forward']") public WebElement checkoutBtn;
+    @FindBy(id = "s2id_billing_country") public WebElement billingCountryDropdown;
+    @FindBy(id = "s2id_autogen1_search") public WebElement billingCountryInputBox;
+    @FindBy(xpath = "//tr[@class='tax-rate tax-rate-in-tax-1']/td") public WebElement taxForIndia;
+    @FindBy(xpath = "//tr[@class='tax-rate tax-rate-roaming-tax-1']/td") public WebElement taxForOtherCountries;
+    @FindBy(xpath = "//td[@class='product-total']/span") public WebElement totalAtLastPage;
 
 }
