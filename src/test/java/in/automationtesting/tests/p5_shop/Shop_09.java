d0826c13b6cc739c0b72a483948e59edc312890e
atinccihad <atinccihad@gmail.com>
2023-08-11 00:03:23 +0300

    automationtesting 0.38.1

--- src/test/java/in/automationtesting/tests/p5_shop/Shop_09.java
+++ src/test/java/in/automationtesting/tests/p5_shop/Shop_09.java
@@ -1,16 +1,21 @@
 package in.automationtesting.tests.p5_shop;
 
+import in.automationtesting.pages.HomePage;
 import in.automationtesting.pages.ShopPage;
 import in.automationtesting.utilities.ConfigurationReader;
 import in.automationtesting.utilities.Driver;
+import in.automationtesting.utilities.ReusableMethods;
 import in.automationtesting.utilities.TestBaseRapor;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertTrue;
 
 public class Shop_09 extends TestBaseRapor {
     //40. Shop-Sale Functionality
     ShopPage shopPage = new ShopPage();
     Actions actions = new Actions(Driver.getDriver());
+    HomePage homepage = new HomePage();
 
     @Test
     public void shopDefaultSortingFunctionality() {
@@ -18,16 +23,20 @@ public class Shop_09 extends TestBaseRapor {
         //1) Open the browser
         //2) Enter the URL “http://practice.automationtesting.in/”
         Driver.getDriver().get(ConfigurationReader.getProperty("base_url"));
-        extentTest.info("Get the url -> http://practice.automationtesting.in/");
 
         //3) Click on Shop Menu
-        shopPage.shopButton.click();
+        homepage.shopBtn.click();
         Driver.getDriver().navigate().refresh();
-        shopPage.shopButton.click();
+        ReusableMethods.waitAndClick(homepage.shopBtn, 3);
         extentTest.info("shopButton tiklandi.");
 
-        //4) Click on Sale written product in home page
+        //4) Click on Sale written product in home page]
+        homepage.saleBtn.click();
+        extentTest.info("saleBtn tiklandi.");
+
         //5) User can clearly view the actual price with old price stricken for the sale written products
+        assertTrue(homepage.actualPrice.isDisplayed() && homepage.oldPrice.isDisplayed());
+        extentTest.info("actualPrice assertion.");
         extentTest.pass("Shop-Sale Functionality test PASS.");
     }
 
