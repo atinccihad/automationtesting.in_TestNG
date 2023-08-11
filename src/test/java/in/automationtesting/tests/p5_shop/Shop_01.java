5d417c219fbe8b3e8ea6aa4b045d21db01766da9
atinccihad <atinccihad@gmail.com>
2023-08-11 20:10:39 +0300

    automationtesting 0.32.1

--- src/test/java/in/automationtesting/tests/p5_shop/Shop_01.java
+++ src/test/java/in/automationtesting/tests/p5_shop/Shop_01.java
@@ -14,7 +14,6 @@ public class Shop_01 extends TestBaseRapor {
     //32. Shop-Filter By Price Functionality
     ShopPage shopPage = new ShopPage();
     Actions actions = new Actions(Driver.getDriver());
-
     @Test
     public void shopFilterByPriceFunctionality() {
         extentTest = extentReports.createTest("Shop_01", "Bos password ile kayit gerceklesmemeli.");
@@ -41,6 +40,7 @@ public class Shop_01 extends TestBaseRapor {
         //6) User can view books only between 150 to 450 rps price
         assertTrue(Driver.getDriver().getCurrentUrl().contains("min_price=150&max_price=450"));
         extentTest.info("User viewed the books, only between 150 to 450 rps price.");
+
         extentTest.pass("Shop-Filter By Price Functionality test PASS.");
     }
 
