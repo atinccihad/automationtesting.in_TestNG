822c91e246444942483511ea41979dd55640033f
atinccihad <atinccihad@gmail.com>
2023-08-11 19:57:49 +0300

    automationtesting 0.21.1

--- src/test/java/in/automationtesting/tests/p3_myAccountLogin/MyAccountLogin_02.java
+++ src/test/java/in/automationtesting/tests/p3_myAccountLogin/MyAccountLogin_02.java
@@ -49,7 +49,6 @@ public class MyAccountLogin_02 extends TestBaseRapor {
         assert homePage.wrongText.isDisplayed();
         extentTest.info("Hata mesaji goruntulendi.");
         Driver.getDriver().navigate().refresh();
-
         extentTest.pass("PASS");
     }
 }
